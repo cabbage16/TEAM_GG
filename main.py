85ee3d25625e4e91f6ec740cdcb436b9cc8c00ed
cabbage16 <baejaehyeon0106@gmail.com>
2023-07-19 23:30:02 +0900

    ADD main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+pg.init()
+
+screen_width = 1280
+screen_height = 720
+pg.display.set_mode((screen_width, screen_height))
+
+pg.display.set_caption("Pygame")
+
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+pg.quit()
