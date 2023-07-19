62d297169a9add67ee497c45e71d33db8678acbf
ghw0223 <ghw2448@gmail.com>
2023-07-20 01:40:52 +0900

    change icon

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ screen_width = 1280
 screen_height = 720
 pg.display.set_mode((screen_width, screen_height))
 
-pg.display.set_caption("Pygame")
+pg.display.set_caption("Good Game")
+
+pg_icon = pg.image.load('#Enter the image')
+pg.display.set_icon(pg_icon)
 
 running = True
 
